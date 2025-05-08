3ae8b38f6cecc1794d4cbdf13b97584104c8453c
Денис <baden.i.ua@gmail.com>
2025-05-08 19:47:01 +0300

    Збираємо без стискання. Бо поки не працює на телефоні.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
 export default {
-  base: '/web-slcan/',
+  // base: '/web-slcan/',
+  base: './',
+  build: {
+    minify: false,
+    terserOptions: {
+      compress: false,
+      mangle: false,
+    },
+  },
 }
